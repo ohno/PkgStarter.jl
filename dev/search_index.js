e44a1c22d95c40fd385f57b86612b33e7e33e4ae
Documenter.jl <documenter@juliadocs.github.io>
2026-01-04 20:18:15 +0000

    build based on be7454f

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#PkgStarter","page":"Home","title":"PkgStarter","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PkgStarter.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#PkgStarter","page":"Home","title":"PkgStarter","text":"Documentation for PkgStarter.\n\n","category":"section"}]
 }
