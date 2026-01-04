e5c5dcba336b6d91fdecb0ddeb3e940268b74d28
Shuhei Ohno <ohno.github@gmail.com>
2026-01-05 05:14:19 +0900

    Update PkgStarter.jl

--- src/PkgStarter.jl
+++ src/PkgStarter.jl
@@ -1,5 +1,13 @@
 module PkgStarter
 
-# Write your package code here.
+const GITHUB_OAUTH_CLIENT_ID = "Ov23libqpCkC6Z5pSlFG"
+const GITHUB_OAUTH_CLIENT_SECRET = "" # Device Flow
+
+function hello()
+    return "Hello, World!"
+end
+
+include("auth.jl")
+include("repo.jl")
 
 end
