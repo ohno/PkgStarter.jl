b494d84eabd1e35a89be92641e7e647b65c589b5
Shuhei Ohno <ohno.github@gmail.com>
2026-02-22 19:03:51 +0900

    Add JET

--- template/test/runtests.jl
+++ template/test/runtests.jl
@@ -1,9 +1,14 @@
 using {{{PKG}}}
 using Aqua
+using JET
 using Test
 
 @testset "Aqua.jl" begin
     Aqua.test_all({{{PKG}}})
+end
+
+@testset "JET.jl" begin
+    JET.test_package({{{PKG}}}; target_modules = ({{{PKG}}},))
 end
 
 @testset "{{{PKG}}}.hello" begin
